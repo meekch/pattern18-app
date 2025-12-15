9bcb5f8d1468999d1abbcf8e269405adbc3aa8c4
Christy Meek <christymeek@yahoo.com>
2025-12-15 08:51:48 -0700

    Fix Stripe API version

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-11-17.clover',
 })
 
 const supabaseAdmin = createClient(
