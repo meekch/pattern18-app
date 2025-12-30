56b7e6a552b2efc24ee4ea69ed502fee35fd3620
Christy Meek <christymeek@yahoo.com>
2025-12-30 05:19:30 -0700

    Fix paperclip and folder emojis

---
+++ fix-two.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let c = fs.readFileSync('src/app/coach/page.tsx', 'utf8');
+c = c.replace(/ðŸ"Ž/g, '📎');
+c = c.replace(/ðŸ" /g, '📁 ');
+fs.writeFileSync('src/app/coach/page.tsx', c, 'utf8');
+console.log('Fixed paperclip and folder!');
